a738d4d0817ebb35b8090f9def7cab0846e47092
Oleh Yurchenko <olegurcenko4@gmail.com>
2023-06-30 01:14:02 +0200

    added all

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+        <img src="https://www.irex.org/sites/default/files/storage-images/IREX_Logo_Grey-H.png"></img>
+      </header>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Nav = () => {
+    return (
+        <nav className='nav-aside'>
+        <div>
+          <a href='#'>Profile</a>
+        </div>
+        <div>
+          <a href='#'>Messages</a>
+        </div>
+        <div>
+          <a href='#'>News</a>
+        </div>
+        <div>
+          <a href='#'>Music</a>
+        </div>
+        <div>
+          <a href='#'>Settings</a>
+        </div>
+      </nav>
+    )
+}
